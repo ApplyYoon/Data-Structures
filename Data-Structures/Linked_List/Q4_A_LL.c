42ff45304ccef08fc9dbaefb5bce93ff9d1a4077
apply <wonji052271@gmail.com>
2025-10-12 01:28:16 +0900

    Successfully solved | Oct. 12, 2025

--- Data-Structures/Linked_List/Q4_A_LL.c
+++ Data-Structures/Linked_List/Q4_A_LL.c
@@ -86,7 +86,26 @@ int main()
 
 void moveEvenItemsToBack(LinkedList *ll)
 {
-	/* add your code here */
+	ListNode *cur;
+	if (ll == NULL) {
+		return;
+	}
+	cur = ll->head;               // first is head
+	
+	int index = 0;
+	int curNode;
+	for (int i = 0; i < ll->size; i++) {
+		curNode = cur->item;      // cur node's item
+		cur = cur->next;          // next node	
+		if (curNode % 2 == 0) {   // if a curNode is even number
+			removeNode(ll, index);
+			insertNode(ll, ll->size, curNode);
+		}
+		else {
+			index++;
+		}
+	}
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
